1f54638e72e2175534768681b5168adc14f83467
Tommy Ngx <tommyfederation@gmail.com>
2023-11-30 07:52:36 +1100

    Update frax code

---
+++ utils.py
@@ -0,0 +1,84 @@
+#
+# selenium 
+# Kora 
+# Update 30 Nov 2023
+#
+# Miscellaneous utilities.
+#
+
+from selenium import webdriver
+from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.common.by import By
+from selenium.webdriver.support.ui import WebDriverWait
+from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.common.action_chains import ActionChains
+from PIL import Image
+from kora.selenium import wd as wd2
+
+os.makedirs("/content/tommy/FRAXphoto", exist_ok=True)
+def findkeyinfo(countryname):
+  countryname = re.findall(r'>(.*?)\<', str(countryname))
+  countryname = re.sub('[^A-Za-z0-9.-]+','', str(countryname))
+  return countryname
+
+def Frax(wd,id, age, weight, height,  prefra, parent, smoke, rheu,  drink, corti, bmd):
+  wd.refresh()
+  wd.get("https://www.sheffield.ac.uk/FRAX/tool.aspx?country=57")
+  # Fill data
+  wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_nameid').clear()
+  wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_nameid').send_keys(id)
+  wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_toolage').clear()
+  wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_toolage').send_keys(age)
+  wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_sex2').click()
+  wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_toolweight').clear()
+  wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_toolweight').send_keys(weight)
+  wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_ht').clear()
+  wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_ht').send_keys(height)
+  wd.find_element(By.XPATH,'//*[@id="dxa"]/option[3]').click()
+  wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_bmd_input').clear
+  wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_bmd_input').send_keys(bmd)
+  if (prefra>0): wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_previousfracture2').click()
+  else:          wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_previousfracture1').click()
+
+  if (parent>0): wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_pfracturehip2').click()
+  else:          wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_pfracturehip1').click()
+
+  if (smoke>1): wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_currentsmoker2').click()
+  else:          wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_currentsmoker1').click()
+
+  if (corti>0): wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_glucocorticoids2').click()
+  else:          wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_glucocorticoids1').click()
+  if (rheu>0): wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_arthritis2').click()
+  else:          wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_arthritis1').click()
+  if (drink>=3): wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_alcohol2').click()
+  else:          wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_alcohol1').click()
+
+  countryname = str(wd.find_element(By.ID,"CountryText"))
+  countryname = findkeyinfo(countryname)
+  sexF = wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_sex2').get_attribute("value")
+  ageF = wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_toolage').get_attribute("value")
+  weighF = wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_toolweight').get_attribute("value")
+  heighF = wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_ht').get_attribute("value")
+  bmdF = wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_bmd_input').get_attribute("value")
+  dxaF = wd.find_element(By.XPATH,'//*[@id="dxa"]').get_attribute("value")
+  time.sleep(2)
+
+  wd.find_element(By.XPATH,'//*[@id="ctl00_ContentPlaceHolder1_btnCalculate"]').click()
+  time.sleep(.31)
+
+  wd.execute_script("window.scrollTo(0, window.scrollY + 180)")
+  wd.save_screenshot('ss1.png')
+  wd.save_screenshot('/content/tommy/FRAXphoto/{}.png'.format(((str(int(id))).zfill(5))))
+  Tscore =  wd.find_element(By.ID,'score')
+  Tscore = findkeyinfo(Tscore)
+  Tscore = Tscore[7:]
+  Majorosteo  = wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_lbrs1')
+  Majorosteo = findkeyinfo(Majorosteo)
+  HipFracture = wd.find_element(By.ID,'ctl00_ContentPlaceHolder1_lbrs2')
+  HipFracture = findkeyinfo(HipFracture)
+
+  print(countryname,((str(int(id))).zfill(5)),"|","Sex:{}|Age:{}|weight:{}|height:{}|BMD:{}|{}|MR:{}|HR:{}".format(sexF,ageF,weighF, heighF,bmdF,Tscore,Majorosteo, HipFracture ) )
+  outputallthing= [int(id), sexF,ageF,weighF, heighF,prefra,parent,smoke,rheu,drink,corti,bmdF,dxaF,Tscore,Majorosteo, HipFracture]
+  return Majorosteo, HipFracture, outputallthing
+
+

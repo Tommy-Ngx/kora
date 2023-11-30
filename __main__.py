361abd631ff55935eccf72b9ac48a246eac8e145
Tommy Ngx <tommyfederation@gmail.com>
2023-12-01 10:17:55 +1100

    Update __main__.py

--- __main__.py
+++ __main__.py
@@ -1,13 +1,15 @@
-# fix code 1 Dec 2023 for Tommy
+# main.py
 
 import argparse
+import pandas as pd
 from utils import download_file_from_google_drive, findkeyinfo, Frax
 from kora.selenium import wd as wd2
 from tqdm import tqdm
+from datetime import datetime
 
 def parse_arguments():
     parser = argparse.ArgumentParser(description="Frax Crawling Script")
-    parser.add_argument("--dataset-dir", required=True, help="Path to the dataset directory")
+    parser.add_argument("--dataset-dir", required=True, help="Path to the CSV dataset file")
     parser.add_argument("--save-to", required=True, help="Path to save the output")
     parser.add_argument("--BMD-column", required=True, help="BMD column name")
     return parser.parse_args()
@@ -15,8 +17,8 @@ def parse_arguments():
 def main():
     args = parse_arguments()
 
-    # Your FraxInput DataFrame loading goes here
-    # For example: FraxInput = pd.read_csv(args.dataset_dir)
+    # Load CSV file using pandas
+    FraxInput = pd.read_csv(args.dataset_dir)
 
     FraxResult = []
 
@@ -50,8 +52,24 @@ def main():
 
         FraxResult.append(outputallthing)
 
-    # Save or print FraxResult as needed
-    #print(FraxResult)
+    # Convert FraxResult to DataFrame
+    df_FraxResult = pd.DataFrame(FraxResult)
+
+    # Rename columns
+    df_FraxResult.rename(columns={
+        0: 'ID', 1: 'GENDER', 2: 'AGE', 3: 'WGHT', 4: 'HGHT', 5: 'FX50', 6: "NFALL", 7: 'ParentF',
+        8: 'SMOKE', 9: 'RHEUMATOID', 10: 'DRINK', 11: 'CORTICOID', 12: args.BMD_column, 13: "DXA",
+        14: f"Tscore_{args.BMD_column}", 15: f"MOsteo_{args.BMD_column}", 16: f"HipFrax_{args.BMD_column}"
+    }, inplace=True)
+
+    # Save the DataFrame to a CSV file
+    current_date = datetime.now().strftime("%d%b%Y")
+    output_csv_path = f'{args.save_to}/FRAX_{args.BMD_column}_{current_date}.csv'
+    df_FraxResult.to_csv(output_csv_path, encoding='utf-8', index=False)
+
+
+    # Print the DataFrame
+    print(df_FraxResult)
 
 if __name__ == "__main__":
     main()

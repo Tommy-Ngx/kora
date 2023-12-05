bd463050b5142e314e4bc3febc383bbf1be69eee
Tommy Ngx <tommyfederation@gmail.com>
2023-12-05 16:24:02 +1100

    Update utils.py

--- utils.py
+++ utils.py
@@ -12,6 +12,7 @@ from selenium.webdriver.common.by import By
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+from selenium.common.exceptions import TimeoutException
 from PIL import Image
 from kora.selenium import wd as wd2
 from tqdm import tqdm
